433585adc36993e32fabca7bb8d51a632791d362
Michael Hopkins <mhopkins321@gmail.com>
2020-10-28 15:37:17 -0500

    Fixing paths

--- init.php
+++ init.php
@@ -1,11 +1,11 @@
 <?php
 // SynapsePay API Resources
-require(dirname(__FILE__) . '/src/SynapsePayRest/api/Node.php');
-require(dirname(__FILE__) . '/src/SynapsePayRest/api/User.php');
-require(dirname(__FILE__) . '/src/SynapsePayRest/api/Trans.php');
-require(dirname(__FILE__) . '/src/SynapsePayRest/api/Subnet.php');
-require(dirname(__FILE__) . '/src/SynapsePayRest/api/Subscription.php');
-require(dirname(__FILE__) . '/src/SynapsePayRest/api/PublicKey.php');
+require(dirname(__FILE__) . '/src/SynapsePayRest/Node.php');
+require(dirname(__FILE__) . '/src/SynapsePayRest/User.php');
+require(dirname(__FILE__) . '/src/SynapsePayRest/Trans.php');
+require(dirname(__FILE__) . '/src/SynapsePayRest/Subnet.php');
+require(dirname(__FILE__) . '/src/SynapsePayRest/Subscription.php');
+require(dirname(__FILE__) . '/src/SynapsePayRest/PublicKey.php');
 // SynapsePay API Resources
 require(dirname(__FILE__) . '/src/SynapsePayRest/HelperFunctions.php');
 require(dirname(__FILE__) . '/src/SynapsePayRest/HttpClient.php');
